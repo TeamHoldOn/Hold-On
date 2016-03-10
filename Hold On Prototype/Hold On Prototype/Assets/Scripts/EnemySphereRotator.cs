b8c90e2bb7840c77d213f1a7a09951ed84b2b416
AubreyMHill <aubrey.hill730@gmail.com>
2016-03-10 10:44:27 -0500

    Prototype Files
    These are the hold on prototype unity files.

---
+++ Hold On Prototype/Hold On Prototype/Assets/Scripts/EnemySphereRotator.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySphereRotator : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (new Vector3 (45, 45, 0) * Time.deltaTime);
+
+	
+	}
+}
