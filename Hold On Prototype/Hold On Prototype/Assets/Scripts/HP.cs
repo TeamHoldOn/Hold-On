895df6b6ecdc73a9c75498b0186a93418404b258
EthanGA <ethan.gossh@gmail.com>
2016-03-15 18:52:23 -0400

    HP
    HP for friendlies

---
+++ Hold On Prototype/Hold On Prototype/Assets/Scripts/HP.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HP : MonoBehaviour {
+
+    public int hitpoints = 2;
+    public GameObject Friendly;
+    public GameObject Enemy;
+    
+ 
+       
+    void OnCollisionEnter(Collision collision) {
+       if ("Enemy" == collision.gameObject.tag) {
+           hitpoints--;
+       }
+    }
+    
+    void Update() {
+        if (hitpoints == 0) {
+           this.Friendly.SetActive(false);
+        }        
+    }
+	
+}
