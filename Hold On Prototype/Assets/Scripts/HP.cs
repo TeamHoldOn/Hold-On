96affd5a56800e8807428614c0b695b662e3794a
EthanGA <ethan.gossh@gmail.com>
2016-03-16 09:59:28 -0400

    HP & Flocking
    added hp and flocking to new master

---
+++ Hold On Prototype/Assets/Scripts/HP.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HP : MonoBehaviour {
+
+    public int hitpoints = 2;
+    public GameObject Friendly;
+         
+    void OnCollisionEnter(Collision collision) {
+       if (collision.gameObject.tag == "Enemy") {
+           hitpoints--;
+       }
+    }
+    
+    void Update() {
+        if (hitpoints == 0) {
+           this.Friendly.SetActive(false);
+        }        
+    }
+	
+}
