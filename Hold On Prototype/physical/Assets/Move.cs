8c344ac95a78d77ef998f86efd500035d5e96d38
AubreyMHill <aubrey.hill730@gmail.com>
2016-03-15 20:09:28 -0400

    Revert "Revert "Master""
    This reverts commit f1e4a39e554e81d58f827ba6e3559882758579cb.

---
+++ Hold On Prototype/physical/Assets/Move.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+
+    public GameObject sphere;
+    public GameObject target;
+    public Transform goal;
+        
+
+	void Start () {
+        sphere.destination = goal.position;
+	}
+	
+
+	void Update () {
+        target.transform.position = sphere.destination;
+        sphere.Move(sphere.destination);
+        if (sphere.destination == target.transform.position)
+        {
+            target.Vector3 = new sphere.destination();
+        } 
+    
+	}
+}
