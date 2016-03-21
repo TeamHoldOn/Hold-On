6edf8c6ec8b2d2c21ddb87a0331a2590ad996d02
austiniyaat <Austin@austinbnionsmbp.american.edu>
2016-03-21 15:53:05 -0400

    improved enemy spawning script
    Created a new script (EnemySpawner) that improves the spawning
    algorithm. Should be attached to an empty game object in the scene.
    Also changed the enemy HP script to take away a half point of hp
    whenever the enemy collides with a friendly

---
+++ Hold On Prototype/Assets/Scripts/SpawnEnemies.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnEnemies : MonoBehaviour {
+
+	public GameObject[] enemies;
+	public int amount;
+	private Vector3 spawnPoint;
+	
+	// Update is called once per frame
+	void Update () {
+		enemies = GameObject.FindGameObjectsWithTag ("Enemy");
+		amount = enemies.Length;
+
+		if (amount < 5) {
+			InvokeRepeating ("spawnEnemy", 5, 10f);
+
+		}
+	
+	}
+
+	void spawnEnemy(){
+		spawnPoint.x = Random.Range(-15, 15);
+		spawnPoint.y = 1.5f;
+		spawnPoint.z = Random.Range(-5, 15);
+
+		Instantiate(enemies[UnityEngine.Random.Range(0, enemies.Length - 1)], spawnPoint, Quaternion.identity);
+		CancelInvoke();
+	}
+			
+}
