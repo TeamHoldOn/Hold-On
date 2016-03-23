897d7d00b86efdbd2e0e89a9cdc07fe1bf484a4a
AubreyMHill <aubrey.hill730@gmail.com>
2016-03-22 20:14:26 -0400

    Beggining to program the terrain
    Adding a blender hex and an empty game object to generate a map into.

---
+++ Hold On Prototype/Assets/Scripts/Map.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Map : MonoBehaviour {
+
+    public GameObject hexPrefab;
+    public int width = 50;
+    public int depth = 40;
+
+
+    void Start() { 
+
+	}
+	
+
+
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
