f2f2d144a7ee592abff4c18dc47ebb842d1d933d
AubreyMHill <aubrey.hill730@gmail.com>
2016-03-23 16:03:09 -0400

    Updating make better flock from master in order to execute pull request

--- Hold On Prototype/Assets/Scripts/HP.cs
+++ Hold On Prototype/Assets/Scripts/HP.cs
@@ -17,6 +17,8 @@ public class HP : MonoBehaviour {
            Friendly.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
            Friendly.GetComponent<Rigidbody>().useGravity = true;
            Destroy (GetComponent<flock>());
+           Friendly.tag = "Untagged";
+           Destroy (Friendly, 3);
         }        
     }
 	
