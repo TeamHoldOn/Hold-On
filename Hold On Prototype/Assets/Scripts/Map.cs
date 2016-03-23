663e2b0822e4cd08f4887547e8f05d08dbeec609
AubreyMHill <aubrey.hill730@gmail.com>
2016-03-22 23:23:19 -0400

    Adding hexes for eventual visual and terrain effects
    Added hex prefab and map object as well as script to generate hex based
    map.

--- Hold On Prototype/Assets/Scripts/Map.cs
+++ Hold On Prototype/Assets/Scripts/Map.cs
@@ -7,8 +7,25 @@ public class Map : MonoBehaviour {
     public int width = 50;
     public int depth = 40;
 
+    float offset = 0.89f;
 
-    void Start() { 
+
+    void Start() {
+        for (int x = 0; x < width; x=(x+2)){
+            for (int z = 0; z < depth; z++)
+            {
+                float xPos = x;
+
+                if ( z % 2 == 1) {
+                    xPos = xPos+offset;
+                }
+
+              GameObject hex_obj =(GameObject)Instantiate(hexPrefab, new Vector3(xPos, 0, z*2*offset), Quaternion.identity);
+                hex_obj.name = "Hex_#_" + x + "_" + z;
+
+                hex_obj.transform.SetParent(this.transform, worldPositionStays:false);
+              }
+        }
 
 	}
 	
