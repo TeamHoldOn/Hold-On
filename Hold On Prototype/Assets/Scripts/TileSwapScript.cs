7d60291b5b3b038ef3bd1240554427133061d136
AubreyMHill <aubrey.hill730@gmail.com>
2016-03-25 22:23:25 -0400

    Beginning Tile Swap Script
    Have script in place is registering collisions isn't destroying hex ...

---
+++ Hold On Prototype/Assets/Scripts/TileSwapScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileSwapScript : MonoBehaviour
+{
+    bool hexhit = false;
+    public GameObject Hex;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Hex")
+        { hexhit = true; }
+
+
+    }
+
+    void Update(){
+        if (hexhit==true) 
+        { 
+         Destroy (Hex, 3f);
+            Debug.Log("Hit the hex");
+        }
+
+    }
+}
