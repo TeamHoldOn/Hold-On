2afea5e704d77c47d5b622fa022ca0dc92e5d6a9
austiniyaat <Austin@AustinBnionsMBP.home>
2016-03-27 00:28:58 -0400

    Merge branch 'master' into PlayerControlPhysicsTesting
    # Conflicts:
    #	Hold On Prototype/Assets/Prefabs/EnemySphere.prefab
    #	Hold On Prototype/Assets/Prefabs/Friendly (1).prefab
    #	Hold On Prototype/Assets/_Scenes/Scene1.unity

---
+++ Hold On Prototype/Assets/Scripts/boundary.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class boundary : MonoBehaviour {
+    
+    public Renderer rend;
+    
+    void Start () {
+        rend = GetComponent<Renderer>();
+        rend.enabled = true;
+    }
+    
+	void OnBecameInvisible () {
+	    rend.enabled = false;
+	}
+	
+	void OnBecameVisisble () {
+	    rend.enabled = true;
+	}
+}
