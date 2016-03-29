2dab68cfddfb02b9831c0748aa59b93e0d35a66c
AubreyMHill <aubrey.hill730@gmail.com>
2016-03-29 01:13:02 -0400

    Should Swap Tiles Trigger enter not detected :,,,,(

--- Hold On Prototype/Assets/Scripts/TileSwapScript.cs
+++ Hold On Prototype/Assets/Scripts/TileSwapScript.cs
@@ -5,18 +5,20 @@ public class TileSwapScript : MonoBehaviour
 {
     bool hexHit = false;
     public GameObject hexagon2;
+    GameObject hexagon;
 
-    void OnCollisionEnter(Collision collision) {
-        if (collision.gameObject.tag == "Friendly") {
-            hexHit = true; 
+    void OnTriggerEnter(Collision trigger) {
+        if (trigger.gameObject.tag == "Dead") {
+            hexHit = true;
+            Debug.Log("Hit the hex"); 
         }
     }
     void Update() {
-        if (hexHit == true) { 
-            Destroy (transform.child="hexagon");
-            Debug.Log("Hit the hex");
-            Instantiate(hexagon2, new Vector3(0, 0, 0), Quaternion.identity);
+        if (hexHit == true) {
+            hexagon = this.gameObject.transform.FindChild("Hexagon").gameObject;
+            Destroy (hexagon);
             
+            GameObject.Instantiate(hexagon2, new Vector3(0, 0, 0), Quaternion.identity);
             hexagon2.transform.SetParent(this.transform, worldPositionStays: true);
         }
     }
