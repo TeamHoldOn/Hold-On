62484ffd578bb6e4c3a8215c5cc9c42d2f290feb
EthanGA <ethan.gossh@gmail.com>
2016-03-30 13:51:54 -0400

    Min distance set!!!!!!!!!!!

--- Hold On Prototype/Assets/Scripts/flock.cs
+++ Hold On Prototype/Assets/Scripts/flock.cs
@@ -4,7 +4,11 @@ using System.Collections;
 public class flock : MonoBehaviour {
  
     private GameObject Leader;
-    public float speed = 3;
+    private GameObject[] Friendlies;
+    
+    void Awake() {
+        Friendlies = GameObject.FindGameObjectsWithTag ("Friendly");
+    }
     
     void OnCollisionEnter(Collision collision) {
        if (collision.gameObject.tag == "Player") {
@@ -18,12 +22,39 @@ public class flock : MonoBehaviour {
        }
     }
     
-    void Update () {
-      foreach (GameObject Friendly in GameObject.FindGameObjectsWithTag("Friendly")) {
-           if (Friendly.tag != "Leader") {
-               Leader = (GameObject.FindWithTag("Leader"));
-               transform.position = Vector3.MoveTowards(transform.position, Leader.transform.position, (Time.deltaTime)/speed);
+    void FixedUpdate() {
+        Flocking();
+        MinimumDistance();
+        
+    }
+    
+    void Flocking () {
+      foreach (GameObject Friendly in Friendlies) {
+        if (Friendly != GameObject.FindWithTag("Leader"))   
+           Leader = (GameObject.FindWithTag("Leader"));
+           Vector3 followLeader = Leader.transform.position - Friendly.transform.position;
+           Rigidbody follow = GetComponent<Rigidbody>();
+           follow.velocity += followLeader.normalized * 0.1f;
+           
+           float speedLimit = follow.velocity.sqrMagnitude;
+           if (speedLimit > 10f) {
+               follow.velocity += -(follow.velocity.normalized); 
            }
       }
+    }
+    
+    void MinimumDistance() {
+        float minDist = 2f;
+        Vector3 selfPosition = transform.position;
+        
+        
+        foreach (GameObject Friendly in Friendlies) {
+            float currentDist = Vector3.Distance (selfPosition, Friendly.transform.position);
+            if (currentDist < minDist) {
+                Vector3 repulse = Friendly.transform.position - selfPosition;
+                Rigidbody avoid = Friendly.GetComponent<Rigidbody>();
+                avoid.velocity += repulse.normalized * 0.3f; 
+            }
+        }
     }           
 }
