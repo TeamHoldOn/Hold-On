769d7b968a316210d155eac5eb4d11026a95b06f
EthanGA <ethan.gossh@gmail.com>
2016-03-29 20:08:43 -0400

    started min dist

--- Hold On Prototype/Assets/Scripts/flock.cs
+++ Hold On Prototype/Assets/Scripts/flock.cs
@@ -4,7 +4,15 @@ using System.Collections;
 public class flock : MonoBehaviour {
  
     private GameObject Leader;
-    public float speed = 3;
+    public GameObject Friendly;
+    private GameObject[] Friendlies;
+    public float speed = 3.0f;
+    public float minDistance = 1.0f;
+    
+    
+    void Start() {
+        Friendlies = GameObject.FindGameObjectsWithTag("Friendly");
+    }
     
     void OnCollisionEnter(Collision collision) {
        if (collision.gameObject.tag == "Player") {
@@ -18,12 +26,22 @@ public class flock : MonoBehaviour {
        }
     }
     
-    void Update () {
-      foreach (GameObject Friendly in GameObject.FindGameObjectsWithTag("Friendly")) {
+    void Update() {
+        foreach (GameObject Friendly in Friendlies) {
            if (Friendly.tag != "Leader") {
                Leader = (GameObject.FindWithTag("Leader"));
                transform.position = Vector3.MoveTowards(transform.position, Leader.transform.position, (Time.deltaTime)/speed);
-           }
-      }
-    }           
+           }    
+        }       
+    }          
+    
+    void LastUpdate() { 
+               Vector3 currentPos = Friendly.transform.position;
+               float currentDist = Vector3.Distance (currentPos, Friendly.transform.position);
+			   if (currentDist < minDistance) {
+			        Vector3 repulseDirection = Friendly.transform.position - currentPos;
+				    Rigidbody collisionRisk = Friendly.GetComponent<Rigidbody>();
+				    collisionRisk.velocity += repulseDirection.normalized * 0.5f;
+               }
+    }
 }
