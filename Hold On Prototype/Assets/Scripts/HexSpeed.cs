4a8ab81ce5b897341c797c2819f0809eeebf1065
AubreyMHill <aubrey.hill730@gmail.com>
2016-03-31 18:13:31 -0400

    Player Speed Script Working
    May need to instead acquire input direction and apply certain amount of
    force in opposite direction rather than modifying speed variable.

---
+++ Hold On Prototype/Assets/Scripts/HexSpeed.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HexSpeed : MonoBehaviour {
+
+    public int envtSpeed = 3;
+	
+}
--- Hold On Prototype/Assets/Scripts/PlayerSpeed.cs
+++ Hold On Prototype/Assets/Scripts/PlayerSpeed.cs
@@ -1,15 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerSpeed : MonoBehaviour {
+public class PlayerSpeed : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    PlayerControllerPhysics playerControllerPhysics;
+    HexSpeed hexSpeed;
+    public int checkedSpeed;
+
+    // this script checks which type of hex the player is on and changes the speed applied to the player as a result
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Hex")
+        {
+            hexSpeed = other.gameObject.GetComponentInChildren<HexSpeed>();
+            checkedSpeed = hexSpeed.envtSpeed;
+            playerControllerPhysics = GetComponent<PlayerControllerPhysics>();
+            playerControllerPhysics.speed = checkedSpeed;
+        }
+    }
 }
