a908c374b45f729f1e9efec84610233fbd4a9d8c
AubreyMHill <aubrey.hill730@gmail.com>
2016-04-03 18:58:52 -0400

    Hexes Imported

---
+++ Hold On Prototype/Assets/Scripts/NearestHexFinder.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NearestHexFinder : MonoBehaviour {
+
+    public GameObject[] nearestHexes;
+    public Transform hexLocation = this.GetComponent<Transform>.transform;
+
+    // Use this for initialization
+
+    private GameObject FindClosestHexes(Vector3 hexLocation)
+    {
+        GameObject nearestHex = null;
+
+        float nearestHexDistance =
+       GameObject[] nearestHexes = GameObject.FindGameObjectsWithTag("Hex");
+
+        foreach (GameObject hex in nearestHexes)
+        {
+            float tempDistance = Vector3.Distance(hexLocation, hex.transform.position);
+
+            if (tempDistance < nearestHexDistance)
+            {
+                nearestHex = hex;
+                nearestHexDistance = tempDistance;
+            }
+        }
+
+        return nearestHex;
+    }
+}
+
+    
+	
