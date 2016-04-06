a51fded95743e283396864e96841bdf4d63ddad8
EthanGA <ethan.gossh@gmail.com>
2016-04-06 19:35:01 -0400

    Desperate Friendlies
    at 1 hp the flock script is destroyed
    
    avoid enemies script is still being worked on

--- Hold On Prototype/Assets/Scripts/HP.cs
+++ Hold On Prototype/Assets/Scripts/HP.cs
@@ -5,6 +5,7 @@ public class HP : MonoBehaviour {
 
     public int hitpoints = 2;
     public GameObject Friendly;
+    public GameObject Enemy;
          
     void OnCollisionEnter(Collision collision) {
        if (collision.gameObject.tag == "Enemy") {
@@ -12,18 +13,33 @@ public class HP : MonoBehaviour {
        }
     }
     
-    void Death() {
-        Friendly.SetActive(false);
-    }
-    
     void Update() {
+        if (hitpoints == 1) {
+           Destroy (GetComponent<flock>());
+           Flee();
+            
+           } 
+                
         if (hitpoints == 0) {
            Friendly.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
            Friendly.GetComponent<Rigidbody>().useGravity = true;
-           Destroy (GetComponent<flock>());
            Friendly.tag = "Dead";
            Invoke("Death", 3);
-        }        
+        }
     }
-	
+        
+    void Flee() {
+        float minDist = 2f;
+        Vector3 selfPosition = transform.position;
+        float currentDist = Vector3.Distance (selfPosition, Enemy.transform.position);
+            if (currentDist < minDist) {
+                Vector3 repulse = Friendly.transform.position - selfPosition;
+                Rigidbody avoid = Friendly.GetComponent<Rigidbody>();
+                avoid.velocity += repulse.normalized * 0.3f; 
+            }
+        }
+        
+    void Death() {
+        Friendly.SetActive(false);
+    }
 }
