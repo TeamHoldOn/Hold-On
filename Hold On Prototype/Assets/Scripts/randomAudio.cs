96a4d8a440ebd08ac7c057397b85a05c09434523
EthanGA <ethan.gossh@gmail.com>
2016-04-06 11:32:27 -0400

    random audio prototype
    random audio system in place, but needs tweaking
    
    also, fixed a piece of code which might cause slowdown

---
+++ Hold On Prototype/Assets/Scripts/randomAudio.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class randomAudio : MonoBehaviour {
+
+	public GameObject player;
+    private Vector3 offset;
+    
+    public AudioSource audio1;
+    public AudioSource audio2;
+    public AudioSource audio3;
+	
+	void Start () {
+		offset = transform.position - player.transform.position;
+	
+	}
+	
+    void Update () {
+        float randomFloat = Random.Range(1f, 4f);
+        if (randomFloat == 1) {
+            audio1.Play();
+        }
+        
+         if (randomFloat == 2) {
+            audio2.Play();
+        }
+        
+         if (randomFloat == 3) {
+            audio3.Play();
+        }
+    }
+    
+	void LateUpdate () {
+		transform.position = player.transform.position + offset;
+	
+	}
+}
