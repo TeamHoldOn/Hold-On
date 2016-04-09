aeac88f9f80fa013b323a1ae289bfdb0c54debb3
AubreyMHill <aubrey.hill730@gmail.com>
2016-04-09 18:09:49 -0400

    MoreSafeZoneScriptFiddling

---
+++ Hold On Prototype/Assets/Scripts/SafeZoneTrigger.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SafeZoneTrigger : MonoBehaviour {
+
+   public static bool safeOut = true;
+   public GameObject player;
+   
+   void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            safeOut = false;
+           Invoke("HaveILeft", 10);
+        }
+    }
+
+    void OnTriggerExit(Collider other){
+            if (other.gameObject.tag == "Player") {
+                safeOut = true;
+            }
+        }
+
+    // Update is called once per frame
+    void HaveILeft()
+    {
+     
+            if (!safeOut)
+            {
+                Debug.Log("Taking Control");
+
+                player.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ |
+                RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
+              
+            }
+     }
+}
