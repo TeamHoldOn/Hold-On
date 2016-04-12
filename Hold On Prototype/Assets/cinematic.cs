a8ec9e3febb54e12c770b8097f2865f411474aac
EthanGA <ethan.gossh@gmail.com>
2016-04-12 19:04:49 -0400

    still working

---
+++ Hold On Prototype/Assets/cinematic.cs
@@ -0,0 +1,32 @@
+﻿/*using UnityEngine;
+using System.Collections;
+
+public class cinematic : MonoBehaviour {
+
+    public GameObject firstProtester;
+
+	void Awake () {
+        
+        GameObject[] firstProtesters = GameObject.FindGameObjectsWithTag("firstProtester");
+        Rigidbody fpRB = firstProtester.GetComponent<Rigidbody>();
+	}
+	
+	void Update () {
+        if (firstProtesters.Length <= 4) {
+            foreach (GameObject firstProtester in firstProtesters) {
+                transform.positition =  
+                
+                if (Friendly != Leader) {
+                    Vector3 followLeader = Leader.transform.position - Friendly.transform.position;
+                    Rigidbody follow = GetComponent<Rigidbody>();
+                    follow.velocity += followLeader.normalized * 0.1f;
+           
+                    float speedLimit = follow.velocity.sqrMagnitude;
+                    if (speedLimit > 1f) {
+                        follow.velocity += -(follow.velocity.normalized); 
+                    }
+            }
+        }
+	}
+}
+
