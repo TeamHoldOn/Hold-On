3bad8f3edc95f5181f1cd22eec351d3ba7208d94
EthanGA <ethan.gossh@gmail.com>
2016-04-12 14:01:31 -0400

    Flock begins stationary
    The flock now begins stationary, and only begins moving once a leader is
    selected (aka first friendly is hit)

--- Hold On Prototype/Assets/Scripts/flock.cs
+++ Hold On Prototype/Assets/Scripts/flock.cs
@@ -3,11 +3,13 @@ using System.Collections;
 
 public class flock : MonoBehaviour {
  
-    private GameObject Leader;
-    private GameObject[] Friendlies;
+    GameObject Leader;
+    GameObject[] Friendlies;
+    GameObject[] Leaders;
     
     void Awake() {
-        Friendlies = GameObject.FindGameObjectsWithTag ("Friendly");
+        Friendlies = GameObject.FindGameObjectsWithTag("Friendly");
+        Leaders = GameObject.FindGameObjectsWithTag("Leader");    
     }
     
     void OnCollisionEnter(Collision collision) {
@@ -17,44 +19,38 @@ public class flock : MonoBehaviour {
                oldLeader.tag = "Friendly";
            }
            
-           gameObject.tag = "Leader";
-           
+           gameObject.tag = "Leader";           
        }
     }
     
+    void Update() {
+        Leaders = GameObject.FindGameObjectsWithTag("Leader");
+    }
+    
     void FixedUpdate() {
-        Flocking();
-        MinimumDistance();
-        
-    }
-    
-    void Flocking () {
-      foreach (GameObject Friendly in Friendlies) {
-        if (Friendly != GameObject.FindWithTag("Leader"))   
-           Leader = (GameObject.FindWithTag("Leader"));
-           Vector3 followLeader = Leader.transform.position - Friendly.transform.position;
-           Rigidbody follow = GetComponent<Rigidbody>();
-           follow.velocity += followLeader.normalized * 0.1f;
-           
-           float speedLimit = follow.velocity.sqrMagnitude;
-           if (speedLimit > 3f) {
-               follow.velocity += -(follow.velocity.normalized); 
-           }
-      }
-    }
-    
-    void MinimumDistance() {
         float minDist = 2f;
-        Vector3 selfPosition = transform.position;
-        
-        
-        foreach (GameObject Friendly in Friendlies) {
-            float currentDist = Vector3.Distance (selfPosition, Friendly.transform.position);
-            if (currentDist < minDist) {
-                Vector3 repulse = Friendly.transform.position - selfPosition;
-                Rigidbody avoid = Friendly.GetComponent<Rigidbody>();
-                avoid.velocity += repulse.normalized * 0.3f; 
+        Vector3 selfPosition = transform.position;  
+        if (Leaders.Length != 0) {
+            foreach (GameObject Friendly in Friendlies) {
+                if (Friendly != GameObject.FindWithTag("Leader")) {
+                    Leader = (GameObject.FindWithTag("Leader"));
+                    Vector3 followLeader = Leader.transform.position - Friendly.transform.position;
+                    Rigidbody follow = GetComponent<Rigidbody>();
+                    follow.velocity += followLeader.normalized * 0.1f;
+           
+                    float speedLimit = follow.velocity.sqrMagnitude;
+                    if (speedLimit > 3f) {
+                        follow.velocity += -(follow.velocity.normalized); 
+                    }
+      
+                    float currentDist = Vector3.Distance (selfPosition, Friendly.transform.position);
+                    if (currentDist < minDist) {
+                        Vector3 repulse = Friendly.transform.position - selfPosition;
+                        Rigidbody avoid = Friendly.GetComponent<Rigidbody>();
+                        avoid.velocity += repulse.normalized * 0.3f; 
+                    }
+                }
             }
         }
-    }           
+    }
 }
