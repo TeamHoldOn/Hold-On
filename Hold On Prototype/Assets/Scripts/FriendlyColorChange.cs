d518a230f8ba8d0a8634f7107403676c6af8ecad
ChongYing <eveline.ycy@gmail.com>
2016-04-13 15:29:30 -0400

    All the color change
    Somehow, in the last merge (I think it’s Aubrey’s background) the color
    change script are all dropped. I fixed here. Hope this time it works.

---
+++ Hold On Prototype/Assets/Scripts/FriendlyColorChange.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FriendlyColorChange : MonoBehaviour {
+	public Color colorOne = new Color();
+	public Color colorTwo = new Color();
+	public Color colorThree = new Color();
+	public Color colorFour = new Color();
+	public Renderer rend;
+	HP hp;
+
+
+	// Use this for initialization
+	void Start () {
+		rend = GetComponent<Renderer>();
+	}
+
+	void Awake (){
+		hp = GetComponent<HP> ();
+	}
+
+
+	void Update() {
+		if (hp.hitpoints >= 3) {
+			rend.material.color = colorOne;
+		}
+		else if(hp.hitpoints >= 2 && hp.hitpoints <3) {
+			rend.material.color = colorTwo;
+		}
+		else if(hp.hitpoints >= 1 && hp.hitpoints <2) {
+			rend.material.color = colorThree;
+		}
+		else {
+			rend.material.color = colorFour;
+		}
+	}
+}
