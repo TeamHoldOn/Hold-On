5d65592227fb3e72fdc2018712bc73ca6a78e422
AubreyMHill <aubrey.hill730@gmail.com>
2016-04-14 16:46:22 -0400

    Created final screen and setting up manager script

---
+++ Hold On Prototype/Assets/Scripts/EndScriptSelect.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class EndScriptSelect : MonoBehaviour
+{
+    public Sprite diedImage;
+    public Sprite fledImage;
+    public Sprite lostImage;
+    Image finalMessage; 
+
+    // Use this for initialization
+    void Start()
+    {
+        if(fled) 
+        {
+         finalMessage = this.GetComponent<Image>();
+            finalMessage.sprite = fledImage;
+        }
+        if (died)
+        {
+            finalMessage = this.GetComponent<Image>();
+            finalMessage.sprite = diedImage;
+
+        }
+        if (lost)
+        {
+
+            finalMessage = this.GetComponent<Image>();
+            finalMessage.sprite = lostImage;
+        }
+    }
+
+}
