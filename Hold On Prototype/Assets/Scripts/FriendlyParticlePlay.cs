61fe9dfb8bd21296f71d327f119ba7e615193690
ChongYing <eveline.ycy@gmail.com>
2016-04-16 19:14:11 -0400

    particle system
    particle system

---
+++ Hold On Prototype/Assets/Scripts/FriendlyParticlePlay.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FriendlyParticlePlay : MonoBehaviour {
+
+		public int checkCurrentHealth;
+		public HP hp;
+
+
+
+		// Use this for initialization
+		void Start () {
+			ParticleSystem ps = GetComponent<ParticleSystem>();
+			ParticleSystem.EmissionModule em = ps.emission;
+			em.enabled = false;
+		}
+
+		// Update is called once per frame
+		void Update () {
+			checkCurrentHealth = hp.hitpoints;
+
+			if (checkCurrentHealth <= 1) {
+				ParticleSystem ps = GetComponent<ParticleSystem> ();
+				ParticleSystem.EmissionModule em = ps.emission;
+				em.enabled = true;
+
+
+			}
+
+			else {
+				ParticleSystem ps = GetComponent<ParticleSystem> ();
+				ParticleSystem.EmissionModule em = ps.emission;
+				em.enabled = false;
+			}
+
+		}
+
+	}
---
+++ Hold On Prototype/Assets/Scripts/ParticlePlay.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticlePlay : MonoBehaviour {
+	
+
+	public int checkCurrentHealth;
+	public PlayerHealth hp;
+
+	// Use this for initialization
+	void Start () {
+		ParticleSystem ps = GetComponent<ParticleSystem>();
+		ParticleSystem.EmissionModule em = ps.emission;
+		em.enabled = false;
+	}
+		
+	// Update is called once per frame
+	void Update () {
+		checkCurrentHealth = hp.currentHealth;
+
+		if (checkCurrentHealth <= 0) {
+			ParticleSystem ps = GetComponent<ParticleSystem> ();
+			ParticleSystem.EmissionModule em = ps.emission;
+			em.enabled = true;
+
+
+			}
+
+		else {
+			ParticleSystem ps = GetComponent<ParticleSystem> ();
+			ParticleSystem.EmissionModule em = ps.emission;
+			em.enabled = false;
+		}
+
+	}
+
+}
