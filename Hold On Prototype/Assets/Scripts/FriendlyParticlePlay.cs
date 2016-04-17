53d9d5ce43701052596092e0c4ae252c48b4ccd1
ChongYing <eveline.ycy@gmail.com>
2016-04-16 20:37:46 -0400

    particle system and expel
    Also a little texture change

---
+++ Hold On Prototype/Assets/Scripts/FriendlyParticlePlay.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FriendlyParticlePlay : MonoBehaviour {
+
+	public int checkCurrentHealth;
+	public HP hp;
+
+	// Use this for initialization
+	void Start () {
+		ParticleSystem ps = GetComponent<ParticleSystem>();
+		ParticleSystem.EmissionModule em = ps.emission;
+		em.enabled = false;
+
+	}
+
+
+
+	// Update is called once per frame
+	void Update () {
+		checkCurrentHealth = hp.hitpoints;
+
+		if (checkCurrentHealth < 2) {
+			ParticleSystem ps = GetComponent<ParticleSystem> ();
+			ParticleSystem.EmissionModule em = ps.emission;
+			em.enabled = true;
+		}
+
+		else {
+			ParticleSystem ps = GetComponent<ParticleSystem> ();
+			ParticleSystem.EmissionModule em = ps.emission;
+			em.enabled = false;
+		}
+
+	}
+
+}
---
+++ Hold On Prototype/Assets/Scripts/PlayerParticlePlay.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerParticlePlay : MonoBehaviour {
+
+		public int checkCurrentHealth;
+		public PlayerHealth hp;
+
+		// Use this for initialization
+		void Start () {
+			ParticleSystem ps = GetComponent<ParticleSystem>();
+			ParticleSystem.EmissionModule em = ps.emission;
+			em.enabled = false;
+
+		}
+
+
+
+		// Update is called once per frame
+		void Update () {
+			checkCurrentHealth = hp.currentHealth;
+
+			if (checkCurrentHealth < 2) {
+				ParticleSystem ps = GetComponent<ParticleSystem> ();
+				ParticleSystem.EmissionModule em = ps.emission;
+				em.enabled = true;
+			}
+
+			else {
+				ParticleSystem ps = GetComponent<ParticleSystem> ();
+				ParticleSystem.EmissionModule em = ps.emission;
+				em.enabled = false;
+			}
+
+		}
+
+	}
