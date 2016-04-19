10da69e8613584ab84ad8c75f988796783aedcf1
EthanGA <ethan.gossh@gmail.com>
2016-04-19 18:49:11 -0400

    Triggers delete when used once
    Added a lot more triggers

--- Hold On Prototype/Assets/Scripts/audioTrigger.cs
+++ Hold On Prototype/Assets/Scripts/audioTrigger.cs
@@ -8,14 +8,22 @@ public class audioTrigger : MonoBehaviour {
     public AudioClip sound;
     AudioSource audio;
     public float volume = 1;
+    bool boom = false;
     
 	void Start() {
 	    audio = GetComponent<AudioSource>();
 	}
     
+    void Update() {
+        if (boom == true) {
+            Destroy (this.gameObject, 3);
+        }
+    }
+    
 	void OnTriggerEnter(Collider player) {
 	    if (player.gameObject.CompareTag("Player")) {
             audio.PlayOneShot(sound, volume);
+            boom = true;
         }
 	}
 }
