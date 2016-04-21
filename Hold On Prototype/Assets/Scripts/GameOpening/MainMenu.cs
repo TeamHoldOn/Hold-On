4a1ba9f817482a5eba08603c39fe501a89c7fd84
AubreyMHill <aubrey.hill730@gmail.com>
2016-04-21 15:44:36 -0400

    Implemented Instruction Screens

--- Hold On Prototype/Assets/Scripts/GameOpening/MainMenu.cs
+++ Hold On Prototype/Assets/Scripts/GameOpening/MainMenu.cs
@@ -1,12 +1,25 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class MainMenu : MonoBehaviour {
 
-	public void PlayGame(){
-		
-		Application.LoadLevel (1);	
+    int continueCounter = 0;
+    public Image instruction;
+    public Sprite englishInstruction;
+    public Sprite arabicInstruction;
 
+    public void PlayGame(){
+
+        continueCounter = (continueCounter + 1);
+        if(continueCounter==1) { 
+        this.instruction.sprite = arabicInstruction; }
+        if (continueCounter == 2) {
+        this.instruction.sprite = englishInstruction; }
+        if (continueCounter == 3)
+        {
+            Application.LoadLevel(1);
+        }
 	}
 
 	public void ExitGame(){
