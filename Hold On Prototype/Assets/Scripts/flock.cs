4fb3990e750185ed7816137f5af6c8b59de6dbdc
EthanGA <ethan.gossh@gmail.com>
2016-06-01 16:59:49 -0400

    Flocking sucks less
    The flock responds to direction changes better. Velocity still needs
    fine tuning, but is also a little better.

--- Hold On Prototype/Assets/Scripts/flock.cs
+++ Hold On Prototype/Assets/Scripts/flock.cs
@@ -4,25 +4,29 @@ using System.Collections;
 public class flock : MonoBehaviour {
  
     GameObject Leader;
+    GameObject Player;
     GameObject[] Friendlies;
     GameObject[] Leaders;
-    bool optimize;
+    bool leaderVel;
+    Rigidbody leaderRB;
     
     void Awake() {
         Friendlies = GameObject.FindGameObjectsWithTag("Friendly");
         Leaders = GameObject.FindGameObjectsWithTag("Leader");
-        optimize = false;    
+        Player = GameObject.FindWithTag("Player");
+        leaderVel = false;
     }
     
     void OnCollisionEnter(Collision collision) {
-       if (collision.gameObject.tag == "Player") {
-           optimize = true;
+       if (collision.gameObject == Player) {
            if (null != GameObject.FindWithTag("Leader")) {
                GameObject oldLeader = GameObject.FindWithTag("Leader");
                oldLeader.tag = "Friendly";
            }
            
-           gameObject.tag = "Leader";           
+           gameObject.tag = "Leader";
+           leaderRB = gameObject.GetComponent<Rigidbody>();
+           leaderVel = true;           
        }
     }
     
@@ -35,28 +39,27 @@ public class flock : MonoBehaviour {
         float minDist = 2f;
         Vector3 selfPosition = transform.position;  
         if (Leaders.Length != 0) {
+            if (leaderVel) {
+                Vector3 leaderVec = Leader.transform.position - Player.transform.position;
+                leaderRB.AddForce(leaderVec.normalized, ForceMode.Impulse);
+                leaderVel = false;
+            }
+                        
             foreach (GameObject Friendly in Friendlies) {
                 if (Friendly != Leader) {
-                    Rigidbody avoid = Friendly.GetComponent<Rigidbody>();
-                    
-                    if (optimize) {
-                        avoid.AddForce(-(Friendly.transform.position), ForceMode.Impulse);
-                        optimize = false;  
-                    }
-                    
                     Vector3 followLeader = Leader.transform.position - Friendly.transform.position;
                     Rigidbody follow = GetComponent<Rigidbody>();
-                    follow.AddForce(followLeader.normalized * 0.3f, ForceMode.Impulse); 
-                    follow.velocity += followLeader.normalized;
+                    follow.velocity += followLeader.normalized * 0.1f;
            
                     float speedLimit = follow.velocity.sqrMagnitude;
                     if (speedLimit > 0.8f) {
-                        follow.velocity += -(follow.velocity.normalized * 1.2f); 
+                        follow.velocity += -(follow.velocity.normalized); 
                     }
       
                     float currentDist = Vector3.Distance (selfPosition, Friendly.transform.position);
                     if (currentDist < minDist) {
                         Vector3 repulse = Friendly.transform.position - selfPosition;
+                        Rigidbody avoid = Friendly.GetComponent<Rigidbody>();
                         avoid.velocity += repulse.normalized * 0.3f;
                     }
                 }
